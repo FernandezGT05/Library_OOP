5f634232e85bd26f620065d159024f21f533c2cb
Tharusha <tharushagayan123@gmail.com>
2025-09-01 16:35:38 +0530

    Wrote the 'books' class and instantiated a book

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 class Books:
-    def __init__(self):
-        pass
+    def __init__(self,title,author,isbn,available):
+        self.title=title
+        self.author=author
+        self.isbn=isbn
+        self.available=available
+        print(f'{self.title} written by {self.author} is {self.available}')
 
 from abc import ABC, abstractmethod
 class Member(ABC):
@@ -18,3 +22,5 @@ class Teacher(Member):
 class Library:
     def __init__(self):
         pass
+
+x=Books("Harry Potter and the Golet of Fire","J.K.Rowling",9780439139595,"available")
