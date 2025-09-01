c4f93313da52526fd81f7077ca07050667474d31
Tharusha <tharushagayan123@gmail.com>
2025-09-01 14:15:50 +0530

    Added the necessary classes

---
+++ main.py
@@ -0,0 +1,20 @@
+class Books:
+    def __init__(self):
+        pass
+
+from abc import ABC, abstractmethod
+class Member(ABC):
+    def __init__(self):
+        pass
+
+class Student(Member):
+    def __init__(self):
+        pass
+
+class Teacher(Member):
+    def __init__(self):
+        pass
+
+class Library:
+    def __init__(self):
+        pass
